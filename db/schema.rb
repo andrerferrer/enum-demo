1e37f9b37958e70824a8e220b4abd3eed2fa6d13
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-23 11:45:51 -0300

    generate schema

---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_23_142052) do
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_on"
+    t.date "end_on"
+    t.integer "customer_id", null: false
+    t.integer "offer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_bookings_on_customer_id"
+    t.index ["offer_id"], name: "index_bookings_on_offer_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "owner_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_id"], name: "index_offers_on_owner_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookings", "offers"
+  add_foreign_key "bookings", "users", column: "customer_id"
+  add_foreign_key "offers", "users", column: "owner_id"
+end
