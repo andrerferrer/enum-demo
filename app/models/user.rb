f12bfb5cb420525b8f40f6347e6c1818687b0612
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-31 13:47:25 -0300

    add associations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :owned_offers, class_name: 'Offer', foreign_key: :owner_id
+  has_many :bookings, foreign_key: :customer_id
+  has_many :purchased_offers, through: :bookings, source: :offer
 end
