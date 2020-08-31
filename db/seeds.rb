9321b1b6a49a72fd7a87eca1c81e1d2b74caa9e4
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-31 13:47:29 -0300

    Create more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 # Create one user
 User.create! first_name: 'Jack', last_name: 'Sparrow', address: 'Black Pearl', phone_number: '123456789'
-
-# Create one offer
-Offer.create! name: 'A trip through the seven seas', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', owner: User.first
-
 # Create another user
 User.create! first_name: 'Elizabeth', last_name: 'Swann', address: 'London', phone_number: '123456789'
 
+# Create one offer
+Offer.create! name: 'A trip through the seven seas', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', owner: User.first
+Offer.create! name: 'A backwards trip through the seven seas', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', owner: User.second
+
+
 # Create a booking
 Booking.create! start_on: '2020-05-23', end_on: '2020-05-30', customer: User.last, offer: Offer.first
+Booking.create! start_on: '2020-05-24', end_on: '2020-06-30', customer: User.first, offer: Offer.last
