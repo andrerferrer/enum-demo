f6c9d540d2d9559ac50b68bc23b259917d858991
andre ferrer <andrerferrer@hotmail.com>
2021-02-13 11:56:06 -0300

    add enum to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :customer, class_name: 'User'
   belongs_to :offer
+
+  enum status: { pending: 0, processing_payment: 1, confirmed: 2, cancelled: -1 }
+  
 end
